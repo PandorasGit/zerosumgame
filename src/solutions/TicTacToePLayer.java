39866265d5a5fdb60ac673a126c85ddced3b866b
PandorasGit <pandorar9000@gmail.com>
2024-03-21 14:41:38 -0400

    Create TicTacToe PLayer

---
+++ src/solutions/TicTacToePLayer.java
@@ -0,0 +1,13 @@
+package solutions;
+
+public class TicTacToePLayer {
+//TODO
+    
+    // while(not terminal)
+        // Print current board
+        // Let Human Player Input (ROW, COLUMN)
+        // RUN MINIMAX SEARCH TO FIND THE BEST MOVE
+        //
+    // announce result (win or draw)
+
+}
